395243cb1b70105bb7b5c7686466eb42cb0456d6
MohamedGamalHG <mohamedgemmyhamzagaber@gmail.com>
2024-07-09 09:51:23 +0300

    feat:add shard package for response when use kafka

--- kafkaResponse/src/main/java/com/cafe/kafka/KafkaResponse.java
+++ kafkaResponse/src/main/java/com/cafe/kafka/KafkaResponse.java
@@ -1,4 +1,4 @@
-package com.cafe.order.kafka.producer;
+package com.cafe.kafka;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class ProductProducerResponse {
+public class KafkaResponse {
     private String productName;
     private int quantity;
 }
